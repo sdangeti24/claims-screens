b5b938c139a3bac90198eaac286a912ada62f867
satish d <satsbelfast@gmail.com>
2022-11-15 20:33:03 +0000

    Draft Version

---
+++ form.js
@@ -0,0 +1,13 @@
+function newClmFrmDisplay(){
+    const ncf = document.getElementById("newClm");
+    const scf = document.getElementById("searchClm");
+      ncf.style.visibility = "visible";
+      scf.style.visibility = "hidden";
+}
+
+function srchClmFrmDisplay(){
+    const ncf = document.getElementById("newClm");
+    const scf = document.getElementById("searchClm");
+      scf.style.visibility = "visible";
+      ncf.style.visibility = "hidden";
+}
